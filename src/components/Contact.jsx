a2a34cb793d290b76964f8fbaba5c83d9142aaeb
shivam chikara <s.chikara6885@gmail.com>
2024-09-22 17:00:43 +0530

    update the keys in contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,8 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -33,8 +35,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_r42ligq",
+        "template_t7xp3b3",
         {
           from_name: form.name,
           to_name: "Shivam Chikara",
@@ -42,7 +44,7 @@ const Contact = () => {
           to_email: "s.chikara6885@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "Gl_U61j1E8cdpW-55"
       )
       .then(
         () => {
