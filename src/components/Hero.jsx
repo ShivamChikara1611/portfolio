0f38508623a849afc962b9363a651a55cb58071a
shivam chikara <s.chikara6885@gmail.com>
2024-09-22 16:37:43 +0530

    update the scroll down element

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = () => {
         <ComputersCanvas/>
       </div>
 
-      <div className="absolute xs:bottom-[50px] bottom-[70px] w-full flex justify-center items-center">
+      <div className="absolute bottom-[100px] w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[30px] h-[50px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
